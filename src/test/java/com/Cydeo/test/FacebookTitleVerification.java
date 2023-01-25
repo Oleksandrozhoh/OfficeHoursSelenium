48ff3fc7c9041811459cb321e94544db76613bbc
Alex_Cydeo <Oleksandrozhoh@gmail.com>
2023-01-25 11:18:54 -0600

    Added Facebook title verification test

--- src/test/java/com/Cydeo/test/FacebookTitleVerification.java
+++ src/test/java/com/Cydeo/test/FacebookTitleVerification.java
@@ -1,9 +1,30 @@
 package com.Cydeo.test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FacebookTitleVerification {
     public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.facebook.com");
+
+        String expectedTitle = "Facebook - log in or sign up";
+        String actualTitle = driver.getTitle();
+
+        if (expectedTitle.equals(actualTitle)){
+            System.out.println("Title verification test - PASSED");
+        }else{
+            System.out.println("Title verification test - FAILED");
+        }
+
+        driver.close();
 
     }
 }
